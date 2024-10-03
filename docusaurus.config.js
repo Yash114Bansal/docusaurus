0c342128230eb001cfa4647431ea362c790d0ab1
lakshay <gupta22154006@akgec.ac.in>
2024-10-03 15:11:57 +0530

    added Rachit Sir

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,6 +86,11 @@ const config = {
             to: '/docs/lakshya',
             position: 'left'
           },
+          {
+            label: 'Rachit Katiyar',
+            to: '/docs/rachit',
+            position: 'left'
+          },
           // {
           //   label: 'About',
           //   to: '/docs/about',
